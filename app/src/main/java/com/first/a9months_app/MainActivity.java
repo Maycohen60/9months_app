1f782e8b3e6a3703cfa321e3e046e36145252f90
Maycohen60 <57867185+Maycohen60@users.noreply.github.com>
2019-11-17 18:47:07 +0200

    Connection between Log in button to home page. Connection between Registration button to registration page

--- app/src/main/java/com/first/a9months_app/MainActivity.java
+++ app/src/main/java/com/first/a9months_app/MainActivity.java
@@ -1,17 +1,17 @@
 package com.first.a9months_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EdgeEffect;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button loginButton;
+
     private Button registrationButton;
     private EditText phoneNum;
     private EditText pass;
@@ -25,11 +25,24 @@ public class MainActivity extends AppCompatActivity {
         pass = (EditText) findViewById(R.id.password);
 
         loginButton= (Button) findViewById(R.id.logButton);
-        registrationButton =(Button) findViewById(R.id.registration);
+        registrationButton =(Button) findViewById(R.id.registrationbutton);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent= new Intent(MainActivity.this, homePage.class);
 
 
+                startActivity(intent);
+            }
+        });
+        registrationButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent= new Intent(MainActivity.this, Registration.class);
+                startActivity(intent);
+            }
+        });
         // take care to login button
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        /*loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -46,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
                 pass.setVisibility(View.VISIBLE);
                 pass.setText(enterd_pass); */
 
-            }
-        });
+         //   }
+       // });
 
 
     }
--- app/src/main/java/com/first/a9months_app/homePage.java
+++ app/src/main/java/com/first/a9months_app/homePage.java
@@ -1,13 +1,26 @@
 package com.first.a9months_app;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class homePage extends AppCompatActivity {
-
+    private Button personalA;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+      personalA= (Button) findViewById(R.id.personalAr);
+
+
+       /* personalA.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent= new Intent(homePage.this, personalArea.class);
+                startActivity(intent);
+            }
+        });*/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
     }
